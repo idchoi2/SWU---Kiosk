ff7b78b569de23f251f8baf23ea34fba546dc577
idchoi2 <idchoi2@gmail.com>
2015-06-05 14:56:51 +0900

    15060401 서울여대 최종 배포 버전 릴리즈 - 빌드 스크립트추가

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+// Gruntfile.js
+module.exports = function(grunt) {
+
+    // Project config
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jslint: {
+            swu: {
+                src: [
+                    'vendor/**/*.js',
+                    'app.js'
+                ],
+                directives: {
+                    nomen: true,
+                    white: true,
+                    browser: true,
+                    sloppy: true,
+                    predef: [
+                        '$', 'jQuery', 'angular', 'confirm', 'alert', 'swu', 'module'
+                    ]
+                }
+            }
+        },
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js'
+            }
+        },
+        jsdoc: {
+            dist: {
+                src: ['README.md'],
+                options: {
+                    encoding: "utf8",
+                    destination: "docs",
+                    recurse: true,
+                    private: true,
+                    configure: 'jsdoc.conf.json'
+                }
+            }
+        },
+        concat : {
+            dist : {
+                src : [
+                    'vendor/script.js',
+                    'app.js'
+                ],
+                dest: 'build/<%= pkg.name %>-<%= pkg.version %>.js'
+            }
+        },
+        uglify : {
+            dist : {
+                files : {
+                    'build/<%= pkg.name %>-<%= pkg.version %>.min.js': ['build/<%= pkg.name %>-<%= pkg.version %>.js']
+                }
+            }
+        }
+    });
+
+
+
+    // Load Plugin
+    grunt.loadNpmTasks('grunt-jslint');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-jsdoc');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+
+
+    grunt.registerTask('default', ['jslint', 'karma', 'jsdoc', 'concat', 'uglify']);
+};
