569627be45d2ae1dbca4074e8d226240eb01e2d7
idchoi2 <idchoi2@gmail.com>
2015-06-05 14:56:21 +0900

    15060401 서울여대 최종 배포 버전 릴리즈 - 빌드 스크립트추가

--- vendor/js/script.js
+++ vendor/js/script.js
@@ -1,5 +1,4 @@
-var swu;
-swu = {};
+
 var video = document.getElementById("video");
 var isPlaying = false;
 var isChanging = false;
@@ -8,6 +7,154 @@ var timeoutInterval = 300000; // 1000 = 1sec
 var forceCloseQ = 5;
 var forceTimer;
 var forceTimeoutInterval = 15000; // 1000 = 1sec
+
+/**
+ * SWU Class
+ */
+var swu;
+swu = {};
+
+/**
+ * 질문 클릭
+ */
+swu.openQsn = function() {
+    isPlaying = true;
+    $("body").mCustomScrollbar("disable");
+    setTimeout(function() {
+        $(".btn-down-ava").show();
+    }, 300);
+
+};
+
+/**
+ * 목록으로
+ */
+swu.closeQsn = function() {
+    isPlaying = false;
+    isChanging = false;
+
+
+
+    $(".btn-up-ava").hide();
+    $(".btn-down-ava").hide();
+
+    setTimeout(function() {
+        $("body").mCustomScrollbar("update");
+        $("#list-container").mCustomScrollbar("scrollTo", "top");
+        $(".btn-toggle-play").removeClass("btn-play");
+        $(".btn-toggle-play").addClass("btn-pause");
+    }, 1000);
+
+};
+
+/**
+ * 재생/일시정지 버튼
+ */
+swu.togglePlay = function(obj) {
+    if(!isChanging) {
+        if (!isPlaying) {
+            $(obj).removeClass("btn-play");
+            $(obj).addClass("btn-pause");
+            document.getElementById("video").play();
+            isPlaying = true;
+        } else {
+            $(obj).removeClass("btn-pause");
+            $(obj).addClass("btn-play");
+            document.getElementById("video").pause();
+            isPlaying = false;
+        }
+    }
+};
+
+/**
+ * 정지
+ */
+swu.stopPlay = function() {
+    if(!isChanging) {
+        $(".btn-toggle-play").removeClass("btn-pause");
+        $(".btn-toggle-play").addClass("btn-play");
+        document.getElementById("video").currentTime = 0;
+        document.getElementById("video").pause();
+        isPlaying = false;
+    }
+};
+
+/**
+ * 영상 변경
+ */
+swu.changeVideo = function() {
+    isChanging = true;
+    setTimeout(function() {
+        $(".btn-toggle-play").addClass("btn-pause");
+        $(".btn-toggle-play").removeClass("btn-play");
+        isPlaying = true;
+        isChanging = false;
+    }, 1500);
+};
+
+/**
+ * 스크롤 업
+ */
+swu.scrollUp = function() {
+    $("#list-container").mCustomScrollbar("scrollTo", "top");
+    $(".btn-up-ava").fadeOut();
+    $(".btn-down-ava").fadeIn();
+};
+
+/**
+ * 스크롤 다운
+ */
+swu.scrollDown = function() {
+    $("#list-container").mCustomScrollbar("scrollTo", "bottom");
+    $(".btn-up-ava").fadeIn();
+    $(".btn-down-ava").fadeOut();
+};
+
+/**
+ * 아무런 동작이 없을시 메인으로 이동
+ */
+swu.timeout = function() {
+    timer = setTimeout(function(){
+
+        $("#wrapper").fadeOut('slow');
+        $("#loading-wrap").fadeIn();
+
+        setTimeout(function() {
+            location.reload();
+        }, 1000);
+    },timeoutInterval);
+};
+
+/**
+ * 강제 종료 버튼
+ */
+swu.forceClose = function() {
+    forceCloseQ = forceCloseQ - 1;
+    // console.log(forceCloseQ);
+    if(!forceCloseQ) {
+        if(confirm("해당 미디어를 종료하시겠습니까?")) {
+
+            // 강제 종료하는 스크립트
+            window.open('', '_self', '');
+            window.close();
+
+            // $("input").focus();
+        } else {
+            forceCloseQ = 5;
+        }
+    }
+};
+
+/**
+ * 15초동안 아무일이 없으면 강제 종료 타이머 초기화
+ */
+swu.forceTimeout = function() {
+    forceTimer = setTimeout(function(){
+        forceCloseQ = 5;
+    },forceTimeoutInterval);
+};
+
+
 
 
 
@@ -118,142 +265,3 @@ $(function () {
 
 
 
-/**
- * 질문 클릭
- */
-swu.openQsn = function() {
-    isPlaying = true;
-    $("body").mCustomScrollbar("disable");
-    setTimeout(function() {
-        $(".btn-down-ava").show();
-    }, 300);
-
-};
-
-/**
- * 목록으로
- */
-swu.closeQsn = function() {
-    isPlaying = false;
-    isChanging = false;
-
-
-
-    $(".btn-up-ava").hide();
-    $(".btn-down-ava").hide();
-
-    setTimeout(function() {
-        $("body").mCustomScrollbar("update");
-        $("#list-container").mCustomScrollbar("scrollTo", "top");
-        $(".btn-toggle-play").removeClass("btn-play");
-        $(".btn-toggle-play").addClass("btn-pause");
-    }, 1000);
-
-};
-
-/**
- * 재생/일시정지 버튼
- */
-swu.togglePlay = function(obj) {
-    if(!isChanging) {
-        if (!isPlaying) {
-            $(obj).removeClass("btn-play");
-            $(obj).addClass("btn-pause");
-            document.getElementById("video").play();
-            isPlaying = true;
-        } else {
-            $(obj).removeClass("btn-pause");
-            $(obj).addClass("btn-play");
-            document.getElementById("video").pause();
-            isPlaying = false;
-        }
-    }
-};
-
-/**
- * 정지
- */
-swu.stopPlay = function() {
-    if(!isChanging) {
-        $(".btn-toggle-play").removeClass("btn-pause");
-        $(".btn-toggle-play").addClass("btn-play");
-        document.getElementById("video").currentTime = 0;
-        document.getElementById("video").pause();
-        isPlaying = false;
-    }
-};
-
-/**
- * 영상 변경
- */
-swu.changeVideo = function() {
-    isChanging = true;
-    setTimeout(function() {
-        $(".btn-toggle-play").addClass("btn-pause");
-        $(".btn-toggle-play").removeClass("btn-play");
-        isPlaying = true;
-        isChanging = false;
-    }, 1500);
-};
-
-/**
- * 스크롤 업
- */
-swu.scrollUp = function() {
-    $("#list-container").mCustomScrollbar("scrollTo", "top");
-    $(".btn-up-ava").fadeOut();
-    $(".btn-down-ava").fadeIn();
-};
-
-/**
- * 스크롤 다운
- */
-swu.scrollDown = function() {
-    $("#list-container").mCustomScrollbar("scrollTo", "bottom");
-    $(".btn-up-ava").fadeIn();
-    $(".btn-down-ava").fadeOut();
-};
-
-/**
- * 아무런 동작이 없을시 메인으로 이동
- */
-swu.timeout = function() {
-    timer = setTimeout(function(){
-
-    $("#wrapper").fadeOut('slow');
-    $("#loading-wrap").fadeIn();
-
-    setTimeout(function() {
-        location.reload();
-    }, 1000);
-    },timeoutInterval);
-};
-
-/**
- * 강제 종료 버튼
- */
-swu.forceClose = function() {
-    forceCloseQ = forceCloseQ - 1;
-    // console.log(forceCloseQ);
-    if(!forceCloseQ) {
-        if(confirm("해당 미디어를 종료하시겠습니까?")) {
-
-            // 강제 종료하는 스크립트
-            window.open('', '_self', '');
-            window.close();
-
-           // $("input").focus();
-        } else {
-            forceCloseQ = 5;
-        }
-    }
-};
-
-/**
- * 15초동안 아무일이 없으면 강제 종료 타이머 초기화
- */
-swu.forceTimeout = function() {
-    forceTimer = setTimeout(function(){
-        forceCloseQ = 5;
-    },forceTimeoutInterval);
-};
